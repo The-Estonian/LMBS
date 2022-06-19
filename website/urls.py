d992b4067ee98f00a4c06aaa8841c0f596cad202
The-Estonian <Zaar2213@gmail.com>
2022-06-19 23:49:54 +0300

    update styling, added posts model+views

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import index
+from .views import index, posts, add_post
 
 urlpatterns = [
     path("", index, name="index"),
+    path("posts/", posts, name="posts"),
+    path("add_post/", add_post, name="add_post"),
 ]
--- website/views.py
+++ website/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from .models import Posts
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 
 def index(request):
     return render(request, "website/index.html")
+
+def posts(request):
+    all_posts = Posts.objects.all()
+    context = {"all_posts":all_posts}
+    return render(request, "website/view_posts.html", context)
+
+def add_post(request):
+    if request.method == "POST":
+        current_poster_id = request.user.id
+        current_poster_instance = User.objects.get(id=current_poster_id)
+        new_post_message = request.POST["new_post"]
+        new_post = Posts.objects.create(user_id=current_poster_instance, message=new_post_message)
+        new_post.save()
+        print(f"User {current_poster_instance.username} posts {new_post_message}")
+    return render(request, "website/add_post.html")
+
